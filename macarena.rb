0ece6302bf996936d9c1f3e869d75226e3404395
Kavya Raghavendhran <rkavyas@gmail.com>
2018-07-23 19:20:31 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -12,7 +12,7 @@ puts "Step 1: Right arm out in front of you, palm facing down."
 puts  "Step 2: Left arm out in front of you, palm facing down."
 
 # Step 3: FIXME
-puts "Step 3: Place your right hand on your left shoulder."
+puts "Step 3: Turn your right palm up."
 
 # Step 4: FIXME
 puts "Step 4: Turn your left palm up."
@@ -27,4 +27,4 @@ puts "Step 6: Left arm out in front of you, palm facing down."
 puts "Step 7: Left arm out in front of you, palm facing down."
 
 # Step 8: FIXME
-puts "Step 8: The Last Step"
+puts "Step 8:  Left arm out in front of you, palm facing down.""
